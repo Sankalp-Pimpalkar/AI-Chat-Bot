a187a0d01aab87e948e86a6f720024a2e1e9ac5f
Sankalp Pimpalkar <SankalpPimpalkar23@gmail.com>
2024-06-05 19:21:44 +0530

    vercel.json added

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: 'esnext',  // Ensure the target is ESNext
-    rollupOptions: {
-      output: {
-        format: 'es',  // Ensure the output format is ES modules
-      },
-    },
-  },
 })

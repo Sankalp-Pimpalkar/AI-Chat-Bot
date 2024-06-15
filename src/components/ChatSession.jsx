d84027a87aedb9152dbbb2566cdc7ffad84d8685
Sankalp Pimpalkar <SankalpPimpalkar23@gmail.com>
2024-06-15 21:32:42 +0530

    ScrollBottom bug fix

--- src/components/ChatSession.jsx
+++ src/components/ChatSession.jsx
@@ -1,88 +1,87 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import databaseService from "../services/appwrite/database"
-import { useSelector } from "react-redux"
-import Message from "./Message"
-import Loader from "./icons/Loader"
-import BottomBar from "./BottomBar"
-import GenerateTextGemini from "../services/gemini/function"
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import databaseService from "../services/appwrite/database";
+import { useSelector } from "react-redux";
+import Message from "./Message";
+import Loader from "./icons/Loader";
+import BottomBar from "./BottomBar";
+import GenerateTextGemini from "../services/gemini/function";
 
 function ChatSession() {
-
-    // Todo : Scroll to bottom is not working properly
-
-    const { sessionId } = useParams()
-    const [chats, setChats] = useState([])
-    const [loadingChats, setLoadingChats] = useState(true)
-    const [loadingPromptResponse, setloadingPromptResponse] = useState(false)
+    const { sessionId } = useParams();
+    const messageRef = useRef();
+    const [chats, setChats] = useState([]);
+    const [loadingChats, setLoadingChats] = useState(false);
+    const [loadingPromptResponse, setLoadingPromptResponse] = useState(false);
     const [message, setMessage] = useState({
         userId: '',
         sessionId,
         sender: 'user',
         message: ''
-    })
-    const userId = useSelector(state => state.authReducer?.userData?.$id)
+    });
+    const userId = useSelector(state => state.authReducer?.userData?.$id);
 
     useEffect(() => {
         if (userId) {
+            setLoadingChats(true);
             (async () => {
-                if (!chats.length) {
-                    const messages = await databaseService.getMessagesBySessionId({
-                        userId,
-                        sessionId
-                    })
+                const messages = await databaseService.getMessagesBySessionId({
+                    userId,
+                    sessionId
+                });
 
-                    if (messages) {
-                        setChats(messages)
-                        setMessage({ ...message, userId })
-                    }
-                    setLoadingChats(false)
+                if (messages) {
+                    setChats(messages);
+                    setMessage(prevMessage => ({ ...prevMessage, userId }));
                 }
-
+                setLoadingChats(false);
             })();
         }
 
-        // Cleanup function to reset state when sessionId changes
         return () => {
             setChats([]);
-            setLoadingChats(true);
         };
-    }, [userId, sessionId])
+    }, [userId, sessionId]);
 
-    async function handleSubmit(e) {
-        e.stopPropagation()
+    useEffect(() => {
+        const container = messageRef.current;
+        if (container) {
+            container.scrollTop = container.scrollHeight;
+        }
+    }, [chats]);
+
+    async function handleSubmit() {
         if (message.message.trim()) {
+            setLoadingPromptResponse(true);
+            const newMessage = { ...message, userId };
+            setChats(prevChats => [...prevChats, newMessage]);
 
-            setloadingPromptResponse(true)
-            setChats(chats => [...chats, message])
-            console.log("Chats ", chats)
-            const inputMessage = message.message
-            // Clearing text input from input field
-            setMessage({ ...message, message: '' })
+            // Clear the input field
+            setMessage(prevMessage => ({ ...prevMessage, message: '' }));
 
-            await databaseService.addNewMessage(message)
+            await databaseService.addNewMessage(newMessage);
 
             const responseFromGemini = await GenerateTextGemini({
-                chatHistory: chats,
-                prompt: inputMessage
-            })
+                chatHistory: [...chats, newMessage],
+                prompt: newMessage.message
+            });
 
             const messageResponseFromGemini = {
                 userId,
                 sessionId,
                 message: String(responseFromGemini),
                 sender: 'model'
-            }
+            };
 
-            setChats(chats => [...chats, messageResponseFromGemini])
-            setloadingPromptResponse(false)
+            setChats(prevChats => [...prevChats, messageResponseFromGemini]);
+            setLoadingPromptResponse(false);
 
-            await databaseService.addNewMessage(messageResponseFromGemini)
+            await databaseService.addNewMessage(messageResponseFromGemini);
         }
     }
 
     function handleChange(inputValue) {
-        setMessage({ ...message, message: inputValue })
+        setMessage(prevMessage => ({ ...prevMessage, message: inputValue }));
     }
 
     if (loadingChats) {
@@ -90,26 +89,27 @@ function ChatSession() {
             <div className="flex justify-center">
                 <Loader className="text-4xl text-blue-500" />
             </div>
-        )
-    } else {
-        return (
-            <div className="w-full md:max-w-6xl h-full pb-20 px-2 flex flex-col gap-8 mx-auto">
-                <div className="w-full h-fit flex flex-col gap-3">
-                    {
-                        chats.map((chat, index) => (
-                            <Message key={index} chats={chats} sender={chat.sender} message={chat.message} />
-                        ))
-                    }
-                </div>
-                <BottomBar
-                    value={message.message}
-                    handleClick={handleSubmit}
-                    handleOnchange={handleChange}
-                    loading={loadingPromptResponse}
-                />
-            </div>
-        )
+        );
     }
+
+    return (
+        <div className="w-full md:max-w-6xl h-full px-2 flex flex-col gap-8 mx-auto">
+            <div ref={messageRef}
+                className="w-full pb-20 h-[calc(100vh-110px)] overflow-y-auto flex flex-col gap-3 hide-scrollbar">
+                {
+                    chats.map((chat, index) => (
+                        <Message key={index} sender={chat.sender} message={chat.message} />
+                    ))
+                }
+            </div>
+            <BottomBar
+                value={message.message}
+                handleClick={handleSubmit}
+                handleOnchange={handleChange}
+                loading={loadingPromptResponse}
+            />
+        </div>
+    );
 }
 
-export default ChatSession
+export default ChatSession;
